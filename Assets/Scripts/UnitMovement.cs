5a69fb662aa009ec8a4b31c323c6afb9f96add9f
Yagi2001 <ramazanbatu2001@outlook.com>
2024-12-27 19:32:45 +0300

    AddedUnitsRunToEachOther

---
+++ Assets/Scripts/UnitMovement.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+public class UnitMovement : MonoBehaviour
+{
+    private GameObject _closestUnit;
+    private Animator _animator;
+    [SerializeField]
+    private float _attackRange = 2f;
+    private string _targetTag;
+    [SerializeField]
+    private float _moveSpeed = 5f;
+    private float _rotationSpeed = 5f;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        if (CompareTag( "AllyUnit" ))
+        {
+            _targetTag = "EnemyUnit";
+        }
+        else if (CompareTag( "EnemyUnit" ))
+        {
+            _targetTag = "AllyUnit";
+        }
+        FindClosestUnit();
+    }
+
+    void Update()
+    {
+        if (_closestUnit != null)
+        {
+            if (Vector3.Distance( transform.position, _closestUnit.transform.position ) > _attackRange)
+            {
+                MoveTowardUnit();
+                RotateTowardsTarget();
+            }
+            if (Vector3.Distance( transform.position, _closestUnit.transform.position ) <= _attackRange)
+            {
+                _animator.SetBool( "isRunning", false );
+            }
+        }
+    }
+
+    void FindClosestUnit()
+    {
+        GameObject[] units = GameObject.FindGameObjectsWithTag( _targetTag );
+        float closestDistance = Mathf.Infinity;
+        foreach (GameObject unit in units)
+        {
+            float distance = Vector3.Distance( transform.position, unit.transform.position );
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                _closestUnit = unit;
+            }
+        }
+    }
+
+    void MoveTowardUnit()
+    {
+        _animator.SetBool( "isRunning", true );
+        if (_closestUnit != null)
+        {
+            Vector3 direction = (_closestUnit.transform.position - transform.position).normalized;
+            transform.position += _moveSpeed * Time.deltaTime * direction;
+        }
+    }
+
+    void RotateTowardsTarget()
+    {
+        if (_closestUnit != null)
+        {
+            Vector3 direction = (_closestUnit.transform.position - transform.position).normalized;
+            if (direction != Vector3.zero)
+            {
+                Quaternion lookRotation = Quaternion.LookRotation( new Vector3( direction.x, 0, direction.z ) );
+                transform.rotation = Quaternion.Slerp( transform.rotation, lookRotation, Time.deltaTime * _rotationSpeed );
+            }
+        }
+    }
+}
